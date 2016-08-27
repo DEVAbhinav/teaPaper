9d4ae80d00502d96a29187673fd1eb6700a76118
Devabhinav <abhinavpandey.1996@gmail.com>
2016-08-28 02:45:04 +0530

    demo get request to a url

--- popup.js
+++ popup.js
@@ -8,85 +8,85 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+// function getCurrentTabUrl(callback) {
+//   // Query filter to be passed to chrome.tabs.query - see
+//   // https://developer.chrome.com/extensions/tabs#method-query
+//   var queryInfo = {
+//     active: true,
+//     currentWindow: true
+//   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+//   chrome.tabs.query(queryInfo, function(tabs) {
+//     // chrome.tabs.query invokes the callback with a list of tabs that match the
+//     // query. When the popup is opened, there is certainly a window and at least
+//     // one tab, so we can safely assume that |tabs| is a non-empty array.
+//     // A window can only have one active tab at a time, so the array consists of
+//     // exactly one tab.
+//     var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+//     // A tab is a plain object that provides information about the tab.
+//     // See https://developer.chrome.com/extensions/tabs#type-Tab
+//     var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+//     // tab.url is only available if the "activeTab" permission is declared.
+//     // If you want to see the URL of other tabs (e.g. after removing active:true
+//     // from |queryInfo|), then the "tabs" permission is required to see their
+//     // "url" properties.
+//     console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
+//     callback(url);
+//   });
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
+//   // Most methods of the Chrome extension APIs are asynchronous. This means that
+//   // you CANNOT do something like this:
+//   //
+//   // var url;
+//   // chrome.tabs.query(queryInfo, function(tabs) {
+//   //   url = tabs[0].url;
+//   // });
+//   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+// }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
+// *
+//  * @param {string} searchTerm - Search term for Google Image search.
+//  * @param {function(string,number,number)} callback - Called when an image has
+//  *   been found. The callback gets the URL, width and height of the image.
+//  * @param {function(string)} errorCallback - Called when the image is not found.
+//  *   The callback gets a string that describes the failure reason.
+ 
+// function getImageUrl(searchTerm, callback, errorCallback) {
+//   // Google image search - 100 searches per day.
+//   // https://developers.google.com/image-search/
+//   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
+//     '?v=1.0&q=' + encodeURIComponent(searchTerm);
+//   var x = new XMLHttpRequest();
+//   x.open('GET', searchUrl);
+//   // The Google image search API responds with JSON, so let Chrome parse it.
+//   x.responseType = 'json';
+//   x.onload = function() {
+//     // Parse and process the response from Google Image Search.
+//     var response = x.response;
+//     if (!response || !response.responseData || !response.responseData.results ||
+//         response.responseData.results.length === 0) {
+//       errorCallback('No response from Google Image search!');
+//       return;
+//     }
+//     var firstResult = response.responseData.results[0];
+//     // Take the thumbnail instead of the full image to get an approximately
+//     // consistent image size.
+//     var imageUrl = firstResult.tbUrl;
+//     var width = parseInt(firstResult.tbWidth);
+//     var height = parseInt(firstResult.tbHeight);
+//     console.assert(
+//         typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
+//         'Unexpected respose from the Google Image Search API!');
+//     callback(imageUrl, width, height);
+//   };
+//   x.onerror = function() {
+//     errorCallback('Network error.');
+//   };
+//   x.send();
+// }
 
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
@@ -94,49 +94,66 @@ function renderStatus(statusText) {
 
 
 function renderArticle(article) {
-  document.getElementById('article').textContent = article.data;
+  document.getElementById('status').textContent = article.data;
 }
+
+function getPdfData(url, callback){
+  var x = new XMLHttpRequest();
+  x.open('GET', url);
+  x.responseType = '';
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    if (!response) {
+      alert('No response from URL');
+      return;
+    }
+    callback(response);
+    //var firstResult = response.responseData.results[0];
+    // Take the thumbnai
+  }
+  x.send();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   renderStatus('Making request for the 3 pages!');
   //url to be defined here by me and passed in getPdfData call --
   //result is the data from the get request to google drive pdf of the newspaper
+  var url = 'http://www.html5rocks.com/en/tutorials/file/xhr2/';
   getPdfData(url, function(result) {
     if(result){
       //scrapedData is the array of objects containing desired information 
       //scraped from the pdf for sample we will use 
       //only 1 article scrapedData[0].data contains information of the article.
-      scrapeData(result, function(scrapedData) {
-        // to be used when displaying all the articles 
-        //for(var i = 0; i<scrapedData.length;i++)
-        renderArticle(scrapeData[0]);
-
-
-      })
+      renderStatus(result);
+      // scrapeData(result, function(scrapedData) {
+      //   // to be used when displaying all the articles 
+      //   //for(var i = 0; i<scrapedData.length;i++)
+      //   renderArticle(scrapeData[0]);
+      // });
     }
-
-  })
-
-
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
   });
 });
+
+  // getCurrentTabUrl(function(url) {
+  //   // Put the image URL in Google search.
+  //   renderStatus('Performing Google Image search for ' + url);
+
+  //   getImageUrl(url, function(imageUrl, width, height) {
+
+  //     renderStatus('Search term: ' + url + '\n' +
+  //         'Google image search result: ' + imageUrl);
+  //     var imageResult = document.getElementById('image-result');
+  //     // Explicitly set the width/height to minimize the number of reflows. For
+  //     // a single image, this does not matter, but if you're going to embed
+  //     // multiple external images in your page, then the absence of width/height
+  //     // attributes causes the popup to resize multiple times.
+  //     imageResult.width = width;
+  //     imageResult.height = height;
+  //     imageResult.src = imageUrl;
+  //     imageResult.hidden = false;
+
+  //   }, function(errorMessage) {
+  //     renderStatus('Cannot display image. ' + errorMessage);
+  //   });
+  // });
